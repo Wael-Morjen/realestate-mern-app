b7e7cef1fb8a9bbf64b461eb220f6c8dd88f7b17
Wael-Morjen <waelmorjen@gmail.com>
2024-01-03 19:33:52 +0100

    redux state management integ

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 const SignIn = () => {
     
     const [formData, setFormData] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const { loading, error } = useSelector((state) => state.user);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleChange = (e) => {
         setFormData({
@@ -19,7 +21,7 @@ const SignIn = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            setLoading(true)
+            dispatch(signInStart());
             const res = await fetch('/api/auth/signin',
                 {
                     method: "POST",
@@ -33,16 +35,13 @@ const SignIn = () => {
             const data = await res.json();
             console.log(data);
             if (data.success === false) {
-                setError(data.message);
-                setLoading(false);
+                dispatch(signInFailure(data.message))
                 return;
             }
-            setLoading(false);
-            setError(false);
+            dispatch(signInSuccess(data));
             navigate('/'); 
         } catch (error) {
-            setLoading(false);
-            setError(error.message);
+            dispatch(signInFailure(error.message));
         }
     }
 
