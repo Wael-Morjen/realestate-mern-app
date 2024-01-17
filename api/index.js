e3a6b7eb3220fd1190f6b56084c03d6fd4bf1f85
Wael-Morjen <waelmorjen@gmail.com>
2024-01-17 19:54:24 +0100

    integrate cookie parser

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 
 import userRouter from './routes/user.route.js'
 import authRouter from "./routes/auth.route.js"
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
