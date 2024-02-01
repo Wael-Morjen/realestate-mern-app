29848956c996dcf4444c9408e3066ac565b032ae
Wael-Morjen <waelmorjen@gmail.com>
2024-02-01 21:12:02 +0100

    share icon & copy link to clipboard function

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import { FaShare } from "react-icons/fa";
 
 
 const Listing = () => {
@@ -12,6 +13,7 @@ const Listing = () => {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
 
   useEffect(() => {
@@ -55,6 +57,23 @@ const Listing = () => {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
         </div>
       )}
     </main>
