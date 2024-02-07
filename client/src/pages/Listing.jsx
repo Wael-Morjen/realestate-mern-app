68942f5b915c99c395dcb3372dc6da0f84239c03
Wael-Morjen <waelmorjen@gmail.com>
2024-02-07 15:44:10 +0100

    add the contact component

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -13,7 +13,9 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -141,6 +143,7 @@ export default function Listing() {
                 Contact landlord
               </button>
             )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
