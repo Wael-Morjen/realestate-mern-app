ccf683d124cf9ecd33bf6bdad76dea6e7d51fe69
Wael-Morjen <waelmorjen@gmail.com>
2024-02-28 20:19:06 +0100

    dependency array

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,13 +21,13 @@ export default function Header() {
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
-  }, [location.search]);
+  }, []);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'>MWM</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
